56cef3f6d27fbbb65e389ccc19b5721c0b2e3697
Snack_les <alignorm@gmail.com>
2026-03-03 11:39:48 +0300

    Update config-file and add README

--- init.el
+++ init.el
@@ -289,7 +289,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :family "Jet Brains Mono Nerd Font"))))
+ '(default ((t (:box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :family "JetBrains Mono"))))
  '(font-lock-keyword-face ((t (:foreground "rosy brown" :weight bold))))
  '(link ((t (:foreground "SlateGray3" :underline t)))))
 (load-theme 'gruvbox)
